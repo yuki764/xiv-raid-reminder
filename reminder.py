678fb588e38fd9956c5bb41d510848b355b48a7b
Setuu <setuu@neigepluie.net>
2021-12-31 01:01:04 +0900

    add bot characteristics feature

--- reminder.py
+++ reminder.py
@@ -28,13 +28,27 @@ with open('config.json', 'r') as config_file:
 
         if 'values' in schedule.keys():
             for row in schedule['values']:
+                # ignore headers
                 if len(row) < 4:
                     continue
 
+                # when today is an activity day
                 if row[1] == today_str and row[3] == '有り':
 
+                    # config the personality of reminder bot
+                    chara = config['reminding_character']
+                    gobi = 'クポ' if chara == 'moogle' else 'です'
+
+                    # get notice text (特記事項)
+                    try:
+                        notice_text = row[12]
+                    except IndexError:
+                        notice_text = ''
+                        print('notice: index error when getting notice_text.')
+
+                    # request to discord
                     payload = {
-                            'content': '今日（' + today_str + '）は' + row[0] + '日クポ。\n特記事項: ' + row[12]
+                            'content': '今日（' + today_str + '）は' + row[0] + '日' + gobi + '。\n特記事項: ' + notice_text
                     }
                     response = requests.post(discord_webhook_url, data=payload)
 
